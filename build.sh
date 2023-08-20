76ec36596778ebe13897857b978a865d52afbc16
Andre Rademacher <andre.rademacher@check24.de>
2023-08-20 18:12:57 +0200

    3 configure golangci-lint

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
+cd "${SCRIPT_DIRECTORY}" || exit
+
+# set options
+# see https://linuxcommand.org/lc3_man_pages/seth.html
+set -o errexit  # abort if command exits on error
+set -o nounset  # abort when a non-existing variable is expanded
+set -o pipefail # abort in case of "pipefail" (one command in pipe fails)
+set -o xtrace   # log all actions and variables
+
+# fetch dependencies in case of removal
+go mod download -x
+
+# run qa analysis
+golangci-lint run
+
+# run test suite
+go test ./...
--- container.sh
+++ container.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
 cd "${SCRIPT_DIRECTORY}" || exit
